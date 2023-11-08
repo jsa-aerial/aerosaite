07ffbb550e74526a6db3300dac9529eae63eef4c
jsa-aerial <jsa.aerial@gmail.com>
2023-11-08 12:41:01 -0500

    enable (at least for some) mkl libs for Win

--- resources/win-runserver.bat
+++ resources/win-runserver.bat
@@ -1,3 +1,5 @@
 @echo off
 
+set PATH=%PATH%;SAITEDIR\Libs
+
 START /B java -cp "SAITEDIR\.saite\resources;SAITEDIR\aerial.aerosaite-VERSION-standalone.jar" aerial.aerosaite.core --port 3000 --repl-port 4100 > start.log 
