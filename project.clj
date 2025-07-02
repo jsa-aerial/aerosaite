b886e3de14953f9f1c8e2f153b63767fcea6883c
jsa-aerial <jsa.aerial@gmail.com>
2025-07-02 19:15:36 -0400

    Update nrepl tooling; saite v 1.6.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aerial.aerosaite "1.6.7"
+(defproject aerial.aerosaite "1.6.9"
   :description
   "Saite for work with Aerobio and sequencing analysis and anything else!"
   :url "https://github.com/aerial/aerobio-saite"
@@ -13,16 +13,17 @@
   :dependencies [[org.clojure/clojure        "1.12.1"]
                  [org.clojure/java.classpath "0.3.0"]
                  [org.clojure/tools.reader   "1.3.6"]
-                 [org.clojure/tools.nrepl    "0.2.13"] ; Explicit nREPL
+                 [nrepl                      "1.3.0"] ; Explicit nREPL
                  [org.clojure/tools.cli      "1.0.206"]
 
-                 [aerial.saite "1.6.7"]
+                 [aerial.saite "1.6.9"]
                  [clj-http     "3.12.3"] ; http client, downloads
                  [cpath-clj    "0.1.2"]  ; JAR resources access
                  ]
 
-  :plugins [[cider/cider-nrepl "0.17.0"]
-            [refactor-nrepl    "2.4.0-SNAPSHOT"]]
+  :plugins [[cider/cider-nrepl "0.50.2"]
+            ;[refactor-nrepl    "3.10.0"]
+            ]
 
   :profiles {:uberjar {:aot :all}}
 
